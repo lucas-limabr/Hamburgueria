f25a0fe81604b2401ec972a719decf8955a4cca0
Lucas Lima <130606633+lucas-limabr@users.noreply.github.com>
2026-04-17 19:57:38 -0300

    upload do projeto inicial

---
+++ hamburgueria/src/main/java/com/designPatterns/hamburgueria/HamburgueriaApplication.java
@@ -0,0 +1,13 @@
+package com.designPatterns.hamburgueria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HamburgueriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HamburgueriaApplication.class, args);
+	}
+
+}
